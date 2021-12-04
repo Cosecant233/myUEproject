41eb4531e96164cb7e80aebbcf2dc7e64921820d
Cosecant <cosecant233@qq.com>
2021-12-04 17:35:03 +0800

    ✨ feat(RealSense): merge RealSense
    将realsense模组加入进来

--- Source/WebSocket.Target.cs
+++ Source/WebSocket.Target.cs
@@ -8,7 +8,7 @@ public class WebSocketTarget : TargetRules
 	public WebSocketTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		//DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "WebSocket" } );
 	}
 }
--- Source/WebSocketEditor.Target.cs
+++ Source/WebSocketEditor.Target.cs
@@ -8,7 +8,7 @@ public class WebSocketEditorTarget : TargetRules
 	public WebSocketEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		//DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "WebSocket" } );
 	}
 }
